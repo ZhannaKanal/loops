7991b3fda1fb36a07811d73e342cf8191f951ee4
Zhanna <kezhanna.03@mail.ru>
2025-12-24 00:49:42 +0500

    task 3

--- longestWordFinder.js
+++ longestWordFinder.js
@@ -1,5 +1,8 @@
 const findLongestWordLength = str => {
   let arr = []
   let splittedString = str.split(" ");
-
+  for(let i =0; i<splittedString.length; i++){
+    let stringLength = splittedString[i].length
+    arr.push(stringLength)
+  }
 }
