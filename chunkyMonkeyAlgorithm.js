39f12073ff245e27c21884a780ae43ded83062d0
Zhanna <kezhanna.03@mail.ru>
2025-12-24 02:02:40 +0500

    task 4

--- chunkyMonkeyAlgorithm.js
+++ chunkyMonkeyAlgorithm.js
@@ -1,4 +1,7 @@
 const chunkArrayInGroups = (arr, num) => {
   let newArr = [];
-  for (let i = 0; i < arr.length; i += num) {}
+  for (let i = 0; i < arr.length; i += num) {
+    let sliced = arr.slice(i, i + num);
+    console.log(sliced);
+  }
 };
