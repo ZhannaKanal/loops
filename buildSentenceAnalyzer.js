0aad465108403c6a76b6a0ae0fced7111baa9efe
Zhanna <kezhanna.03@mail.ru>
2025-12-23 23:25:52 +0500

    task 4

--- buildSentenceAnalyzer.js
+++ buildSentenceAnalyzer.js
@@ -12,3 +12,17 @@ const getVowelCount = (sentence) => {
 
 const vowelCount = getVowelCount("Apples are tasty fruits");
 console.log(`Vowel Count: ${vowelCount}`);
+
+const getConsonantCount = sentence => {
+  const consonants = "bcdfghjklmnpqrstvwxz";
+  let lowerCased = sentence.toLowerCase();
+  let counter = 0;
+  for(const char of lowerCased){
+    if(consonants.includes(char)){
+      counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(getConsonantCount("HhH"))
