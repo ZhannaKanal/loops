05b14bb2f83a4a10fbff64534234d983b02730b7
Zhanna <kezhanna.03@mail.ru>
2025-12-23 23:37:42 +0500

    task 9

--- buildSentenceAnalyzer.js
+++ buildSentenceAnalyzer.js
@@ -13,32 +13,32 @@ const getVowelCount = (sentence) => {
 const vowelCount = getVowelCount("Apples are tasty fruits");
 console.log(`Vowel Count: ${vowelCount}`);
 
-const getConsonantCount = sentence => {
+const getConsonantCount = (sentence) => {
   const consonants = "bcdfghjklmnpqrstvwxyz";
   let lowerCased = sentence.toLowerCase();
   let counter = 0;
-  for(const char of lowerCased){
-    if(consonants.includes(char)){
+  for (const char of lowerCased) {
+    if (consonants.includes(char)) {
       counter++;
     }
   }
   return counter;
-}
+};
 
-const consonantCount = getConsonantCount("Coding is fun")
-console.log(`Consonant Count: ${consonantCount}`)
+const consonantCount = getConsonantCount("Coding is fun");
+console.log(`Consonant Count: ${consonantCount}`);
 
-const getPunctuationCount = sentence => {
+const getPunctuationCount = (sentence) => {
   const notPunctuations = " qwertyuiopasdfghjklzxcvbnm";
   let counter = 0;
-  for(const char of sentence.toLowerCase()){
-    if(!notPunctuations.includes(char)){
+  for (const char of sentence.toLowerCase()) {
+    if (!notPunctuations.includes(char)) {
       counter++;
-      console.log(char)
+      console.log(char);
     }
   }
   return counter;
-}
+};
 
 const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
-console.log(`Punctuation Count: ${punctuationCount}`)
+console.log(`Punctuation Count: ${punctuationCount}`);
