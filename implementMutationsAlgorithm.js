512122ebf9f014ed5ca2b5548d37dea426904dbe
Zhanna <kezhanna.03@mail.ru>
2025-12-24 01:37:42 +0500

    task 7

--- implementMutationsAlgorithm.js
+++ implementMutationsAlgorithm.js
@@ -1,17 +1,17 @@
-const mutation = arr => {
-  let newArr = []
+const mutation = (arr) => {
+  let newArr = [];
   for (let i = 0; i < arr.length; i++) {
-    const lowered = arr[i].toLowerCase()
-    newArr.push(lowered)
+    const lowered = arr[i].toLowerCase();
+    newArr.push(lowered);
   }
-  for(const char of newArr[1]){
-    if(!newArr[0].includes(char)){
+  for (const char of newArr[1]) {
+    if (!newArr[0].includes(char)) {
       return false;
     }
   }
   return true;
-
-}
-const arr = ["zyxwvutsrqponmlkjihgfedcba", "qrstu"]
-const test = mutation(arr)
-console.log(test)
+};
+const arr = ["zyxwvutsrqponmlkjihgfedcba", "qrstu"];
+const arr2 = ["ate", "date"];
+const test = mutation(arr);
+console.log(test);
