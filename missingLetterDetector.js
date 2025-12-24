e82bd1d6e2d722e2cd842389e1789e20ebbf507c
Zhanna <kezhanna.03@mail.ru>
2025-12-25 01:46:38 +0500

    task 4

--- missingLetterDetector.js
+++ missingLetterDetector.js
@@ -1,7 +1,7 @@
 const fearNotLetter = (str) => {
   for (let i = 0; i < str.length - 1; i++) {
     if (str[i + 1].charCodeAt() - str[i].charCodeAt() > 1) {
-      return false
+      const result = str[i].charCodeAt() + 1;
     }
   }
 };
