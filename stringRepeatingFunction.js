5762447c1f3cb43c6761475a13adb842ae8b840d
Zhanna <kezhanna.03@mail.ru>
2025-12-25 00:59:22 +0500

    task 3

--- stringRepeatingFunction.js
+++ stringRepeatingFunction.js
@@ -1,3 +1,8 @@
-const repeatStringNumTimes = (str, num) =>{
-    let arr = [];
-}
+const repeatStringNumTimes = (str, num) => {
+  let arr = [];
+  if (!num <= 0) {
+    return num
+  } else {
+    return "";
+  }
+};
